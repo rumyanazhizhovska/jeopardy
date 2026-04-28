23a4b1674940c1c2d163e77f8eeb4d64f3d168e4
Rumyana <rumyanaz@stud.ntnu.no>
2026-04-28 18:34:15 +0200

    bare en commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
 import "./App.css";
+import questionsData from './questions.json';
+// questionsData.categories er nå en liste med alle kategoriene
+import Board from './components/Board';
 
 function App() {
+  return (
+        <div className="game-screen">
+            <h1 className="game-title">🎯 Jeopardy</h1>
+            <Board />
+        </div>
+    );
+    
   return (
     <div className="card">
       <h1>Velkommen! 👋</h1>
---
+++ src/components/Board.jsx
@@ -0,0 +1,8 @@
+import questionsData from './questions.json';
+// questionsData.categories er nå en liste med alle kategoriene
+
+function Board() {
+    return <div>Hei, jeg er brettet!</div>;
+}
+
+export default Board;
